e3d696430c18522e5d8ff678f29b09ddc838f7f4
Kennlf <kenn741q@stud.kea.dk>
2022-04-25 20:22:15 +0200

    Emploeey and departmentsworking

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,7 +1,21 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Employee;
+import com.example.demo.repositories.EmployeeRepository;
+import com.example.demo.repositories.IRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
+
+    private final IRepository<Employee> employeeIRepository = new EmployeeRepository();
+
+
+    @GetMapping("/employees")
+    public String allEmployees(Model model){
+        model.addAttribute("employees", employeeIRepository.getAllEntities());
+        return "employees";
+    }
 }
--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -1,4 +1,73 @@
 package com.example.demo.models;
 
+import java.sql.Date;
+
 public class Employee {
+
+
+    private int id;
+    private String employeeName;
+    private String job;
+    private int manager;
+    private Date hireDate;
+    private int salary;
+    private int commission;
+    private int departmentNumber;
+
+    public Employee(int id, String employeeName, String job, int manager, Date hireDate, int salary, int commission, int departmentNumber) {
+        this.id = id;
+        this.employeeName = employeeName;
+        this.job = job;
+        this.manager = manager;
+        this.hireDate = hireDate;
+        this.salary = salary;
+        this.commission = commission;
+        this.departmentNumber = departmentNumber;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getEmployeeName() {
+        return this.employeeName;
+    }
+
+    public String getJob() {
+        return this.job;
+    }
+
+    public int getManager() {
+        return this.manager;
+    }
+
+    public Date getHireDate() {
+        return this.hireDate;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int getCommission() {
+        return this.commission;
+    }
+
+    public int getDepartmentNumber() {
+        return this.departmentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", employeeName='" + employeeName + '\'' +
+                ", job='" + job + '\'' +
+                ", manager=" + manager +
+                ", hireDate=" + hireDate +
+                ", salary=" + salary +
+                ", commission=" + commission +
+                ", departmentNumber=" + departmentNumber +
+                '}';
+    }
 }
